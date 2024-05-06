d5abc262788f967afc163663cb26b84fee9e0714
Mahaut Windal <mwindal@hotmail.com>
2024-05-06 09:56:56 +0200

    Entité Book SQL Dialect

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,18 @@
 module com.example.bookproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.base;
     requires org.controlsfx.controls;
     requires java.persistence;
+    requires java.sql;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+
 
     opens com.example.bookproject to javafx.fxml;
     exports com.example.bookproject;
+    opens com.example.bookproject.Model to org.hibernate.orm.core;
+    exports com.example.bookproject.Model;
+    exports org.hibernate.mydialect to org.hibernate.orm.core;
+
 }
